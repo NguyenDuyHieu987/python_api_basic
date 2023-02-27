bdddd1339843445da3849a494fb89b144086f5e4
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-02-27 13:37:30 +0700

    add

--- main.py
+++ main.py
@@ -3,19 +3,30 @@ import json
 import pymongo
 from waitress import serve
 from gevent.pywsgi import WSGIServer
+from bson import json_util, ObjectId
 
 
 myclient = pymongo.MongoClient("mongodb+srv://admin:hieusen123@cluster0.h6ko2rq.mongodb.net/movie_web_dev?retryWrites=true&w=majority")
 
+mydb = myclient["movie_web_dev"]
+movies = mydb["movies"]
+tvs = mydb["movies"]
+
+
+
 app = Flask(__name__)
 
-@app.route('/', methods=[ 'GET'])
-def home_page():
-    data_set = {'name':'hiếu'}
-    json_dump = json.dumps(data_set)
-    
+@app.route('/movie', methods=[ 'GET'])
+def movies_route():
+    json_dump = json.loads(json_util.dumps(movies.find_one()))
+    return json_dump
+
+@app.route('/tv', methods=[ 'GET'])
+def tvs_route():
+    json_dump = json.loads(json_util.dumps(tvs.find_one()))
     return json_dump
 
 if __name__ == '__main__':
-    http_server = WSGIServer(('', 777), app)
+    # app.run(port=7777)
+    http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
