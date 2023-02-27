3c0595f483cfeb2ac1ea94e21d8ecdb30e069d91
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-02-27 13:11:52 +0700

    add

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from flask import *
 import json
+import pymongo
+from waitress import serve
+from gevent.pywsgi import WSGIServer
+
+
+myclient = pymongo.MongoClient("mongodb+srv://admin:hieusen123@cluster0.h6ko2rq.mongodb.net/movie_web_dev?retryWrites=true&w=majority")
 
 app = Flask(__name__)
 
@@ -11,4 +17,5 @@ def home_page():
     return json_dump
 
 if __name__ == '__main__':
-    app.run(port=7777)
+    http_server = WSGIServer(('', 777), app)
+    http_server.serve_forever()
