74cb289619d5ebcca8c0af6d98c40fb2e807c53b
NguyenDuyHieu987 <vaicut6941@gmail.com>
2023-02-27 17:08:06 +0700

    add

--- main.py
+++ main.py
@@ -12,19 +12,23 @@ mydb = myclient["movie_web_dev"]
 movies = mydb["movies"]
 tvs = mydb["movies"]
 
-
-
 app = Flask(__name__)
 
 @app.route('/movie', methods=[ 'GET'])
 def movies_route():
-    json_dump = json.loads(json_util.dumps(movies.find_one()))
+    json_dump = json.loads(json_util.dumps(movies.find({})))
     return json_dump
 
 @app.route('/tv', methods=[ 'GET'])
 def tvs_route():
     json_dump = json.loads(json_util.dumps(tvs.find_one()))
     return json_dump
+
+
+@app.route('/img', methods=[ 'GET'])
+def img_route():
+    return send_file("./images/628548.jpg", mimetype='image/gif')
+
 
 if __name__ == '__main__':
     # app.run(port=7777)
